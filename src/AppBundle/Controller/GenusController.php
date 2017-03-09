b3c1a3063c60cb44067d071e393225cd5543c2b2
kariolos <l.g.karolos@gmail.com>
2017-03-09 17:17:36 +0200

    json response v1.0

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -3,9 +3,11 @@
 namespace AppBundle\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 // To get access to the service container you have to extend Symfony's baseController
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 
@@ -20,16 +22,33 @@ class GenusController extends Controller
     public function showAction($genusName)
     {
 
-        $notes = [
-            '// Each object has a key ->mailer -> mailer object',
-            '// Each object has a key ->mailer -> mailer object',
-            '// Each object has a key ->mailer -> mailer object'
-        ];
-
         return $this->render('genus/show.html.twig',[
             'name' => $genusName,
             'notes' => $notes
         ]);
 
+    }
+
+    // Method get --Without this route will match this request using any http method (post,delete,get) Optional
+    /**
+     * @Route("/genus/{genusName}/notes")
+     * @Method("GET")
+     */
+    public function getNotesAction()
+    {
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        // json structure
+        $data = [
+            'notes' => $notes
+        ];
+
+        //it calls json_encode
+        //it sets application/json content type header on the response
+        return new JsonResponse($data);
     }
 }
