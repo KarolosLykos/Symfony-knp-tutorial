7ee7b24c3b5f10375b53c4eea84675d4b7eb34ce
kariolos <l.g.karolos@gmail.com>
2017-03-22 12:24:32 +0200

    End of forms

--- src/AppBundle/Form/GenusFormType.php
+++ src/AppBundle/Form/GenusFormType.php
@@ -37,10 +37,10 @@ class GenusFormType extends AbstractType
                 ]
             ])
             ->add('firstDiscoveredAt', DateType::class, [
-//                'widget' => 'single_text',
-//                'attr' => [
-//                    'class' => 'js-datepicker'
-//                ],
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'js-datepicker'
+                ],
                 'html5' => 'false'
             ]);
     }
